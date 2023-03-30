bc5daaa434772cf0d1b3c632b761cf1ec3d017ff
Kushal <kushaldeveloper@gmail.com>
2023-03-30 06:19:45 -0500

    add a model class

---
+++ Models/MyOrder.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Models
+{
+    public class MyOrder
+    {
+        [property: JsonPropertyName("myOrderId")]
+        public int MyOrderId { get; set; }
+
+        [property: JsonPropertyName("myOrderName")]
+        public string MyOrderName { get; set; } = "avocado on rye";
+    }
+}
